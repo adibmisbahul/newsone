ae6472376c989fd62a3683238b17705d51f47ebf
ADIB MISBAHUL ULUM <adibmisbahul101812@gmail.com>
2024-09-02 22:50:31 +0800

    pppp

--- src/Components/Fragment/CardNews.jsx
+++ src/Components/Fragment/CardNews.jsx
@@ -1,33 +1,33 @@
 import { useState, useEffect } from "react";
 
 const CardNews = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  // const [data, setData] = useState(null);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    // Define an async function to fetch data
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/news");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   // Define an async function to fetch data
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch("http://localhost:3000/news");
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       const data = await response.json();
+  //       setData(data);
+  //     } catch (error) {
+  //       setError(error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    // Call the async function
-    fetchData();
-  }, []); // Empty dependency array means this effect runs once when the component mounts
+  //   // Call the async function
+  //   fetchData();
+  // }, []); // Empty dependency array means this effect runs once when the component mounts
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error: {error.message}</p>;
 
   return (
     <div key={data.id}>
